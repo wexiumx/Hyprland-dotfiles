13bd0a38420b1a8f9cef68b6606673fcec14a130
wexiumx <lukas.bludnickas@proton.me>
2026-02-28 15:03:20 +0200

    added waybar config for 2k monitor & maded it bigger

--- waybar/.config/waybar/scripts/waybar-wttr.py
+++ waybar/.config/waybar/scripts/waybar-wttr.py
@@ -1,115 +1,34 @@
 #!/usr/bin/env python
-
 import json
 import requests
-from datetime import datetime
 
-WEATHER_CODES = {
-    '113': '☀️',
-    '116': '⛅️',
-    '119': '☁️',
-    '122': '☁️',
-    '143': '🌫',
-    '176': '🌦',
-    '179': '🌧',
-    '182': '🌧',
-    '185': '🌧',
-    '200': '⛈',
-    '227': '🌨',
-    '230': '❄️',
-    '248': '🌫',
-    '260': '🌫',
-    '263': '🌦',
-    '266': '🌦',
-    '281': '🌧',
-    '284': '🌧',
-    '293': '🌦',
-    '296': '🌦',
-    '299': '🌧',
-    '302': '🌧',
-    '305': '🌧',
-    '308': '🌧',
-    '311': '🌧',
-    '314': '🌧',
-    '317': '🌧',
-    '320': '🌨',
-    '323': '🌨',
-    '326': '🌨',
-    '329': '❄️',
-    '332': '❄️',
-    '335': '❄️',
-    '338': '❄️',
-    '350': '🌧',
-    '353': '🌦',
-    '356': '🌧',
-    '359': '🌧',
-    '362': '🌧',
-    '365': '🌧',
-    '368': '🌨',
-    '371': '❄️',
-    '374': '🌧',
-    '377': '🌧',
-    '386': '⛈',
-    '389': '🌩',
-    '392': '⛈',
-    '395': '❄️'
+ICONS = {
+    0: "☀️", 1: "🌤", 2: "⛅", 3: "☁️",
+    45: "🌫", 48: "🌫",
+    51: "🌦", 53: "🌦", 55: "🌧",
+    61: "🌦", 63: "🌧", 65: "🌧",
+    71: "🌨", 73: "🌨", 75: "❄️",
+    80: "🌦", 81: "🌧", 82: "🌧",
+    95: "⛈", 96: "⛈", 99: "⛈"
 }
 
-data = {}
-
-
-weather = requests.get("https://wttr.in/?format=j1").json()
-
-
-def format_time(time):
-    return time.replace("00", "").zfill(2)
-
-
-def format_temp(temp):
-    return (hour['FeelsLikeC']+"°").ljust(3)
-
-
-def format_chances(hour):
-    chances = {
-        "chanceoffog": "Fog",
-        "chanceoffrost": "Frost",
-        "chanceofovercast": "Overcast",
-        "chanceofrain": "Rain",
-        "chanceofsnow": "Snow",
-        "chanceofsunshine": "Sunshine",
-        "chanceofthunder": "Thunder",
-        "chanceofwindy": "Wind"
+r = requests.get(
+    "https://api.open-meteo.com/v1/forecast",
+    params={
+        "latitude": 54.6872,
+        "longitude": 25.2797,
+        "current": "temperature_2m,apparent_temperature,weathercode,windspeed_10m",
+        "timezone": "Europe/Vilnius"
     }
+).json()
 
-    conditions = []
-    for event in chances.keys():
-        if int(hour[event]) > 0:
-            conditions.append(chances[event]+" "+hour[event]+"%")
-    return ", ".join(conditions)
+c = r["current"]
+icon = ICONS.get(c["weathercode"], "?")
+temp = round(c["temperature_2m"])
+feels = round(c["apparent_temperature"])
+wind = round(c["windspeed_10m"])
 
-
-data['text'] = WEATHER_CODES[weather['current_condition'][0]['weatherCode']] + \
-    " "+weather['current_condition'][0]['FeelsLikeC']+"°"
-
-data['tooltip'] = f"<b>{weather['current_condition'][0]['weatherDesc'][0]['value']} {weather['current_condition'][0]['temp_C']}°</b>\n"
-data['tooltip'] += f"Feels like: {weather['current_condition'][0]['FeelsLikeC']}°\n"
-data['tooltip'] += f"Wind: {weather['current_condition'][0]['windspeedKmph']}Km/h\n"
-data['tooltip'] += f"Humidity: {weather['current_condition'][0]['humidity']}%\n"
-for i, day in enumerate(weather['weather']):
-    data['tooltip'] += f"\n<b>"
-    if i == 0:
-        data['tooltip'] += "Today, "
-    if i == 1:
-        data['tooltip'] += "Tomorrow, "
-    data['tooltip'] += f"{day['date']}</b>\n"
-    data['tooltip'] += f"⬆️ {day['maxtempC']}° ⬇️ {day['mintempC']}° "
-    data['tooltip'] += f"🌅 {day['astronomy'][0]['sunrise']} 🌇 {day['astronomy'][0]['sunset']}\n"
-    for hour in day['hourly']:
-        if i == 0:
-            if int(format_time(hour['time'])) < datetime.now().hour-2:
-                continue
-        data['tooltip'] += f"{format_time(hour['time'])} {WEATHER_CODES[hour['weatherCode']]} {format_temp(hour['FeelsLikeC'])} {hour['weatherDesc'][0]['value']}, {format_chances(hour)}\n"
-
-
-print(json.dumps(data))
-
+print(json.dumps({
+    "text": f"{icon} {temp}°",
+    "tooltip": f"{icon} {temp}°C  feels like {feels}°C  💨 {wind} km/h"
+}))
